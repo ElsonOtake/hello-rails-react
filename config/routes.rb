6a83e3b12f63e89bd1c7dd9ecca73978a2b67948
Elson Otake <elsonotake@yahoo.com.br>
2022-10-17 19:28:05 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "root#index"
-  namespace :api, defaults: { format: :json } do
+  namespace :api do
     namespace :v1 do
-      resources :greetings, only: %i[index]
+      resources :greetings, only: %i[index], defaults: { format: :json }
     end
   end
+  root "root#index"
 end
