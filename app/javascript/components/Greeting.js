0abbf6907d35020d042d52daa611aed84b25ed47
Elson Otake <elsonotake@yahoo.com.br>
2022-10-17 19:52:46 -0300

    Create api fetch and display

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Greeting = () => (
-  <div>
-    <h1>Greeting</h1>
-  </div>
-);
+const Greeting = () => {
+  const [greetings, setGreetings] = useState([]);
+
+  const loadData = async () => {
+    const response = await axios.get('/api/v1/greetings');
+    setGreetings(response.data.text);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <>
+      <h1>{greetings}</h1>
+    </>
+  );
+}
 
 export default Greeting;
