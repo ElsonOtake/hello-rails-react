da9e69665915352570917c20e71b6bd02cbd161b
Elson Otake <elsonotake@yahoo.com.br>
2022-10-17 23:28:54 -0300

    Update link to root

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import App from './components/App';
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
 );
