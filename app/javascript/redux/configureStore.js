667220501420b12becbbcf8300d1bc37af9cb04e
Elson Otake <elsonotake@yahoo.com.br>
2022-10-17 22:10:11 -0300

    Add store

---
+++ app/javascript/redux/configureStore.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { greetingReducer } from './greeting';
+
+const store = configureStore({ reducer: greetingReducer });
+
+export default store;
